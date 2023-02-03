93b5089c504099e488c5ad6a93f75ecfd3d99509
Momar Mbengue <momar.mbengue@terinnova.com>
2023-02-03 12:21:33 +0000

    add jenkins file

--- reservation-service/src/main/java/com/momarious/reservationservice/controller/ReservationController.java
+++ reservation-service/src/main/java/com/momarious/reservationservice/controller/ReservationController.java
@@ -7,17 +7,17 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class ReservationController {
 	
-    @Value("${message}")
-	private String message;    
+    // @Value("${message}")
+	// private String message;    
 
     @GetMapping("/")
     public String home() {
         return "Welcome to the Reservation service";
     }
 
-    @GetMapping("/message")
-    public String message() {
-        return message;
-    }
+    // @GetMapping("/message")
+    // public String message() {
+    //     return message;
+    // }
 
 }
--- user-service/src/main/java/com/momarious/userservice/controller/UserController.java
+++ user-service/src/main/java/com/momarious/userservice/controller/UserController.java
@@ -7,17 +7,17 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class UserController {
 
-    @Value("${message}")
-	private String message;    
+    // @Value("${message}")
+	// private String message;    
 
     @GetMapping("/")
     public String home() {
         return "Welcome to the User service";
     }
 
-    @GetMapping("/message")
-    public String message() {
-        return message;
-    }
+    // @GetMapping("/message")
+    // public String message() {
+    //     return message;
+    // }
 
 }
